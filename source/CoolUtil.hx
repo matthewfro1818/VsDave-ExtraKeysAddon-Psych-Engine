adad925609b17206dd267beed8281bac77aa9123
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-11-27 17:50:02 -0500

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -138,4 +138,16 @@ class CoolUtil
 		FlxG.openURL(site);
 		#end
 	}
+	public static function getMinAndMax(value1:Float, value2:Float):Array<Float>
+	{
+		var minAndMaxs = new Array<Float>();
+
+		var min = Math.min(value1, value2);
+		var max = Math.max(value1, value2);
+
+		minAndMaxs.push(min);
+		minAndMaxs.push(max);
+		
+		return minAndMaxs;
+	}
 }
